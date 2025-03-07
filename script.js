c2121b9b8c66d77a1f54aa1fa479ddda05b7aae2
Kotyara45 <nazarivanyuk562@gmail.com>
2025-03-07 18:31:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -7,5 +7,10 @@ window.onload = () => {
 
     setTimeout(() => {
         document.querySelector('.container').style.animation = 'fadeOut 2s forwards';
-    }, 5000);
+    }, 5000); 
+
+    
+    setTimeout(() => {
+        window.location.href = 'fs19.html'; 
+    }, 7000);
 };
